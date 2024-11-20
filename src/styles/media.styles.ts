7e2b0850574305441a576f44d02546c9b63e4720
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-21 04:11:33 +0900

    🔥 안쓰는 media.styles 함수 삭제

--- src/styles/media.styles.ts
+++ src/styles/media.styles.ts
@@ -1,21 +1,4 @@
-// src/styles/Common/media.ts
 import { css, Interpolation, DefaultTheme } from 'styled-components';
-
-export const responsiveGridColumns = {
-  desktop: css`
-    grid-template-columns: repeat(3, 1fr);
-  `,
-  tablet: css`
-    @media screen and (min-width: 768px) and (max-width: 1248px) {
-      grid-template-columns: repeat(2, 1fr);
-    }
-  `,
-  mobile: css`
-    @media screen and (max-width: 767px) {
-      grid-template-columns: repeat(1, 1fr);
-    }
-  `,
-};
 
 export const media = {
   ta: (
