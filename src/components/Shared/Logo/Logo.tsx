d1f8112fa37b4ec94ead596fd9f0071159e13b59
tare <59001439+junghwa1996@users.noreply.github.com>
2024-11-21 04:05:53 +0900

    🔨 로고 컴포넌트에서 스타일 관리

--- src/components/Shared/Logo/Logo.tsx
+++ src/components/Shared/Logo/Logo.tsx
@@ -1,21 +1,38 @@
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import { StyledLogo } from './Logo.styles';
+import { Link } from 'react-router-dom';
+import { media } from '../../../styles/media.styles';
 
 import LOGO_PC from '/logo.png';
 import LOGO_MO from '/logo_mo.png';
 
-const Logo = () => {
+type LogoProps = {
+  size: 'sm' | 'md';
+};
+
+function Logo({ size = 'sm' }: LogoProps) {
   return (
-    <StyledLogo>
+    <StyledLogo size={size}>
       <Link to={'/'}>
         <picture>
-          <source srcSet={LOGO_MO} media='(max-width: 767px)' />
+          {size === 'sm' && (
+            <source srcSet={LOGO_MO} media='(max-width: 767px)' />
+          )}
           <img src={LOGO_PC} alt='로고' />
         </picture>
       </Link>
     </StyledLogo>
   );
-};
+}
+
+export const StyledLogo = styled.h1<LogoProps>`
+  width: ${({ size }) => (size === 'sm' ? '15.3rem' : '39.6rem')};
+  height: ${({ size }) => (size === 'sm' ? '5.1rem' : '13.2rem')};
+
+  ${media.mo`
+    width: ${({ size }) => (size === 'sm' ? '8.1rem' : '19.8rem')};
+    height: ${({ size }) => (size === 'sm' ? '2.7rem' : '6.6rem')};
+  `}
+`;
 
 export default Logo;
